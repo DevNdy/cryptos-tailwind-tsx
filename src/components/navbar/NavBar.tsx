8c1c368945dc1ff7787613c9bd9a673e095cefa1
DevNdy <developpeur.ndy@gmail.com>
2022-10-11 11:24:27 +0200

    F01 feat NAVBAR > Add fixed

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -4,7 +4,7 @@ import NavBarRightSide from "./NavBarRightSide";
 
 const NavBar = () => {
   return (
-    <header className="bg-slate-900 h-14 w-screen flex flex-row justify-between ">
+    <header className="bg-slate-900 h-14 w-screen flex flex-row justify-between fixed ">
       <NavBarLeftMenu />
       <NavBarRightSide />
     </header>
