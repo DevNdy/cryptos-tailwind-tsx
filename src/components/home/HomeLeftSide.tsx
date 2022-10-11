f9445f28ae22d04047bbadf9d3b990487245d3ee
DevNdy <developpeur.ndy@gmail.com>
2022-10-11 11:51:05 +0200

    F02 feat HOMELEFT > Create HomeLeft

---
+++ src/components/home/HomeLeftSide.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const HomeLeftSide = () => {
+  return (
+    <article className="flex flex-col w-[35%] text-gray-100 ml-20">
+      <h2 className="text-3xl mb-5">Bienvenue,</h2>
+      <h4 className="text-xl mb-4">La crypto c'est quoi?</h4>
+      <p className="italic font-light">
+        En résumé, la cryptomonnaie est une forme d’argent qui n'existe que sur Internet. La plupart
+        des cryptomonnaies sont générées par ce qu’on appelle une “blockchain” (chaîne de blocs).
+        Imaginez qu’une blockchain est similaire à un registre de transactions stockées sur une
+        foule d’ordinateurs différents dans le monde entier, lesquels actualisent constamment ce
+        registre. Chaque ordinateur, ou machine, est appelé noeud (ou node), et vérifie constamment
+        que sa version du registre est similaire à celle des autres. Ainsi, toute tentative
+        d’altération ou modification malhonnête est facilement détectée et supprimée.
+      </p>
+    </article>
+  );
+};
+
+export default HomeLeftSide;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import HomeLeftSide from "../components/home/HomeLeftSide";
 
 const HomePage = () => {
-  return <div className="bg-slate-900 min-h-[90vh]">hello home</div>;
+  return (
+    <div className="bg-slate-900 min-h-[100vh] flex flex-col justify-center">
+      <div className="flex flex-row justify-between">
+        <HomeLeftSide />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
